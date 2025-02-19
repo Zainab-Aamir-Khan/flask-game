d6b60867054333f5a89fe26283254c929058c593
Zainab Aamir <zaamir427@gmail.com>
2025-02-19 21:52:03 +0500

    practice of flask

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello world!!!</h1>"
+    return "<h1>My life is filled with colors!! yayee!!!!!</h1>"
 
 if __name__ == '__main__':
     app.run(debug =True)
